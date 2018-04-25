a33bdc589023f9ce571a940b8648b4f4306e4804
Maximiliano Horta <maximilianohorta94@gmail.com>
2018-04-25 13:17:48 +0200

    i3 blockets

--- i3/.i3/blockets/mail.sh
+++ i3/.i3/blockets/mail.sh
@@ -1,5 +1,5 @@
 USER=maximilianohorta94
-PASS=$(gpg2 -dq ~/.mutt/gmail_pwds.gpg)
+PASS=$(gpg2 -dq ~/.mutt/accounts/gmail_pwds.gpg)
 
 COUNT=`curl -su $USER:$PASS https://mail.google.com/mail/feed/atom || echo "<fullcount>unknown number of</fullcount>"`
 COUNT=`echo "$COUNT" | grep -oPm1 "(?<=<fullcount>)[^<]+" `
