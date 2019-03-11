5ce47386dce2f68141eb2b2653cc1bf110b9d3bb
Maximiliano Horta <maximilianohorta94@gmail.com>
2019-03-11 13:41:49 +0100

    added script to autosave in addressbook

--- mutt/.config/neomutt/scripts/alias.sh
+++ mutt/.config/neomutt/scripts/alias.sh
@@ -4,7 +4,9 @@ MESSAGE=$(cat)
 
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($2)"-"tolower($(NF-1)) $0;}}')
 
-if grep -Fxq "$NEWALIAS" $HOME/.config/neomutt/aliases; then
+MAIL=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{print $NF}')
+
+if grep -Fq "$MAIL" $HOME/.config/neomutt/aliases; then
     :
 else
     echo "$NEWALIAS" >> $HOME/.config/neomutt/aliases
